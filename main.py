effe05a96efbb2b53bd5057920843cd70c3b4ca2
ogidog <ogidog@mail.ru>
2022-03-28 00:07:46 +0700

    Home commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+
+template = {}
+
+
+def read_tamplate_file():
+    print()
+
+
+def main(template_file="TEMPLATE.txt"):
+    print(template_file)
+
+
+if __name__ == "__main__":
+    [template_file] = sys.argv[1:]
+
+    main(template_file)
