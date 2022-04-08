7fce8939950d13c7e48d310146c46d06460c553e
ogidog <ogidog@mail.ru>
2022-04-09 00:17:52 +0700

    Home commit

--- houghAnalysis/houghAnalysis.py
+++ houghAnalysis/houghAnalysis.py
@@ -1,6 +1,20 @@
+import numpy as np
+from scipy.stats import norm
+import matplotlib.pyplot as plt
 from houghAnalysis.plot_inHoughFrame import plot_inHoughFrame
 
+
 def houghAnalysis(nodes):
+    plt.close()
 
     nodes = plot_inHoughFrame(nodes)
+    [mu, sigma] = norm.fit(np.array(nodes['ori_mean_deg']).reshape(-1, 1))
+    print('Gaussian distribution parameters for orientation: \n mu={} -- sigma={}\n'.format(mu, sigma))
+
+    # Apparent spacing
+    app_spacing = np.empty((1, len(nodes['r']) - 1))
+    app_spacing[:] = np.NaN
+    r_theta = np.hstack((np.array(nodes['r']).reshape(-1,1), np.array(nodes['ori_mean']).reshape(-1,1)))
+    r_theta = r_theta[np.argsort(r_theta[:,0])]
+
     pass
--- houghAnalysis/plot_inHoughFrame.py
+++ houghAnalysis/plot_inHoughFrame.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
 from read_write_joints.polylines_to_lines import polylines_to_lines
 
 
@@ -10,4 +12,13 @@ def plot_inHoughFrame(nodes):
     plt.xlabel('Theta')
     plt.ylabel('r')
 
+    nodes['r'] = [*range(len(nodes['iD']))]
+    for j in range(len(nodes['iD'])):
+        x = nodes['line'][j][0]
+        y = nodes['line'][j][2]
+        nodes['r'][j] = np.abs(y*np.cos(np.radians(nodes['ori_mean_deg'][j])) - x*np.sin(np.radians(nodes['ori_mean_deg'][j])))
+        plt.plot(nodes['ori_mean_deg'][j], nodes['r'][j], 'r.')
+
     plt.show()
+
+    return nodes
